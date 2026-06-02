55d7905e6f7b7ee964d1c57bb7b299b50930bfc6
evisikurnia <evisikurnia@gmail.com>
2026-06-02 22:32:09 +0700

    Tugas Pertemuan 11

---
+++ app/View/Components/BukuCard.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class BukuCard extends Component
+{
+    // Daftarkan variabel agar bisa diakses di view blade komponen
+    public $buku;
+    public $showActions;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($buku, $showActions = true)
+    {
+        $this->buku = $buku;
+        $this->showActions = $showActions;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.buku-card');
+    }
+}
